29b30c79741c51265df76f8936ce2c984178c0d4
Nicholas Meyers <nicholas.meyers@hotmail.com>
2024-05-24 00:43:42 +0200

    feat: honk and flash

--- src/main/java/be/nicholas/api/flash/mapper/FlashMapper.java
+++
@@ -1,8 +0,0 @@
-package be.nicholas.api.flash.mapper;
-
-import be.nicholas.api.flash.domain.Flash;
-import be.nicholas.api.resource.FlashWebResponseResource;
-
-public interface FlashMapper {
-    FlashWebResponseResource toWebResource(final Flash flash);
-}
---
+++ src/main/java/be/nicholas/api/honk/service/HonkClientService.java
@@ -0,0 +1,8 @@
+package be.nicholas.api.honk.service;
+
+import be.nicholas.api.honk.domain.Honk;
+
+public interface HonkClientService {
+
+    Honk honk(Honk honk);
+}

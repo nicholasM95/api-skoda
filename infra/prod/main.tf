029ecbdde38f5570469d151874e33d64c67b6aae
Nicholas Meyers <nicholas.meyers@hotmail.com>
2024-06-13 00:02:40 +0200

    deps: update tf modules

--- infra/prod/main.tf
+++ infra/prod/main.tf
@@ -5,14 +5,14 @@ resource "random_uuid" "vault_secret_id" {
 }
 
 module "dns" {
-  source    = "git::https://github.com/nicholasM95/terraform-modules.git//modules/dns-cloudflare?ref=v1.1.5"
+  source    = "git::https://github.com/nicholasM95/terraform-modules.git//modules/dns-cloudflare?ref=v1.1.6"
   ip        = var.server_ip
   name      = "api-skoda"
   host_name = var.host_name
 }
 
 module "application" {
-  source           = "git::https://github.com/nicholasM95/terraform-modules.git//modules/k8s-helm-release?ref=v1.1.5"
+  source           = "git::https://github.com/nicholasM95/terraform-modules.git//modules/k8s-helm-release?ref=v1.1.6"
   image_tag        = var.image_tag
   application_name = "api-skoda"
   namespace_name   = "skoda"

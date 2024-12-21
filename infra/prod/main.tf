4ac92b9392348523851ac25a464963ed173147eb
Nicholas Meyers <nicholas.meyers@hotmail.com>
2024-12-21 19:55:47 +0100

    chore: remove dns record

--- infra/prod/main.tf
+++ infra/prod/main.tf
@@ -1,10 +1,3 @@
-module "dns" {
-  source    = "git::https://github.com/nicholasM95/terraform-modules.git//modules/dns-cloudflare?ref=v1.8.3"
-  ip        = var.server_ip
-  name      = "api-skoda"
-  host_name = var.host_name
-}
-
 module "application" {
   source           = "git::https://github.com/nicholasM95/terraform-modules.git//modules/k8s-helm-release?ref=v1.8.3"
   image_tag        = var.image_tag

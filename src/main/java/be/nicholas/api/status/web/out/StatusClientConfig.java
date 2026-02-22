e0c35fc7c54ae106e166e81436b9db8f58f571ea
Nicholas Meyers <nicholas.meyers@one16.be>
2026-02-22 23:05:36 +0100

    chore: refactor object mapper config

--- src/main/java/be/nicholas/api/status/web/out/StatusClientConfig.java
+++ src/main/java/be/nicholas/api/status/web/out/StatusClientConfig.java
@@ -2,6 +2,7 @@ package be.nicholas.api.status.web.out;
 
 import be.nicholas.api.core.error.ApiErrorDecoder;
 import be.nicholas.api.core.web.AuthInterceptor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import feign.Feign;
@@ -26,6 +27,7 @@ public class StatusClientConfig {
     private ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return objectMapper;
     }
 }
